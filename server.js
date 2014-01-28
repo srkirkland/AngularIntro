63603051042212cb2d98ee050329ce0337cbb01d
Scott Kirkland <srkirkland@gmail.com>
2014-01-28 11:31:42 -0800

    back to ejs for angular support

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 var express = require('express'),
-    exphbs  = require('express3-handlebars'),
 
     app = express();
-
-app.set('views', __dirname + '/views');
-app.engine('.html', exphbs({defaultLayout: 'main', extname: '.html'}));
-app.set('view engine', '.html');
-
-//app.use(express.favicon());
   
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home.ejs');
+});
+
+app.get('/basic', function (req, res) {
+    res.render('basic.ejs');
 });
 
 app.listen(process.env.PORT || 3000);
