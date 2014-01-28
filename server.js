ca582cc2cfb5d0c2ac8457245e7460d71cbb6c7c
Scott Kirkland <srkirkland@gmail.com>
2014-01-28 11:50:22 -0800

    added list example

--- server.js
+++ server.js
@@ -10,4 +10,8 @@ app.get('/basic', function (req, res) {
     res.render('basic.ejs');
 });
 
+app.get('/list', function (req, res) {
+    res.render('list.ejs');
+});
+
 app.listen(process.env.PORT || 3000);
